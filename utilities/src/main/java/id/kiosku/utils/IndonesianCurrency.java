d2eb0ad9024b150aa3e85cf8632dc4df5ba274e8
Silverius Dodi <silveriusdodi@x-dev.id>
2019-06-24 21:20:28 +0700

    Add float and double data type in indonesian currency

--- utilities/src/main/java/id/kiosku/utils/IndonesianCurrency.java
+++ utilities/src/main/java/id/kiosku/utils/IndonesianCurrency.java
@@ -120,6 +120,82 @@ public class IndonesianCurrency {
         }
     }
 
+    public static String get(Float plain){
+        if(plain!=null) {
+            DecimalFormat format = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+            DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+
+            symbols.setCurrencySymbol("Rp ");
+            symbols.setMonetaryDecimalSeparator(',');
+            symbols.setGroupingSeparator('.');
+
+            format.setDecimalFormatSymbols(symbols);
+
+            return format.format(plain);
+        }else{
+            return null;
+        }
+    }
+
+    public static String getWithFree(Float plain) {
+        if(plain!=null) {
+            if (plain > 0) {
+                DecimalFormat format = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+                DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+
+                symbols.setCurrencySymbol("Rp ");
+                symbols.setMonetaryDecimalSeparator(',');
+                symbols.setGroupingSeparator('.');
+
+                format.setDecimalFormatSymbols(symbols);
+
+                return format.format(plain);
+            } else {
+                return "Free";
+            }
+        }else{
+            return null;
+        }
+    }
+
+    public static String get(Double plain){
+        if(plain!=null) {
+            DecimalFormat format = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+            DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+
+            symbols.setCurrencySymbol("Rp ");
+            symbols.setMonetaryDecimalSeparator(',');
+            symbols.setGroupingSeparator('.');
+
+            format.setDecimalFormatSymbols(symbols);
+
+            return format.format(plain);
+        }else{
+            return null;
+        }
+    }
+
+    public static String getWithFree(Double plain) {
+        if(plain!=null) {
+            if (plain > 0) {
+                DecimalFormat format = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+                DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+
+                symbols.setCurrencySymbol("Rp ");
+                symbols.setMonetaryDecimalSeparator(',');
+                symbols.setGroupingSeparator('.');
+
+                format.setDecimalFormatSymbols(symbols);
+
+                return format.format(plain);
+            } else {
+                return "Free";
+            }
+        }else{
+            return null;
+        }
+    }
+
     public static String get(String plain){
         if(plain!=null) {
             try{
