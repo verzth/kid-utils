3bdb636a7d5932d467ba4723ca720c0b318c5870
Silverius <silveriuz16@gmail.com>
2019-08-19 10:30:44 +0700

    update demo and version release

--- demo/src/main/java/id/kiosku/utils/demo/MainActivity.java
+++ demo/src/main/java/id/kiosku/utils/demo/MainActivity.java
@@ -2,6 +2,7 @@ package id.kiosku.utils.demo;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -17,8 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if(checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED){
-            Log.e("Lat", LocationDriver.getInstance().getLatitude()+"");
-            Log.e("Long", LocationDriver.getInstance().getLongitude()+"");
+            LocationDriver.getInstance().watch(new LocationDriver.WatchLocation() {
+                @Override
+                public void onUpdate(Location location) {
+                    Log.e("Lat", LocationDriver.getInstance().getLatitude()+"");
+                    Log.e("Long", LocationDriver.getInstance().getLongitude()+"");
+                    Log.e("Admin", LocationDriver.getInstance().getAddress(LocationDriver.AddressType.ADMIN));
+                    Log.e("SubAdmin", LocationDriver.getInstance().getAddress(LocationDriver.AddressType.SUBADMIN));
+                    Log.e("Locality", LocationDriver.getInstance().getAddress(LocationDriver.AddressType.LOCALITY));
+                    Log.e("SubLocality", LocationDriver.getInstance().getAddress(LocationDriver.AddressType.SUBLOCALITY));
+                    Log.e("ADD1", LocationDriver.getInstance().getAddress(LocationDriver.AddressType.ADDRESS));
+                    Log.e("ADD2", LocationDriver.getInstance().getAddress(LocationDriver.AddressType.ADDRESS_SECOND));
+                    Log.e("NAME", LocationDriver.getInstance().getAddress(LocationDriver.AddressType.NAME));
+                    Log.e("Thoroughfare", LocationDriver.getInstance().getAddress(LocationDriver.AddressType.THOROUGHFARE));
+                }
+            });
         }else if (Build.VERSION.SDK_INT >= 23){
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},100);
         }
@@ -28,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         LocationDriver.getInstance().triggerService();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocationDriver.getInstance().unwatch();
     }
 
     @Override
